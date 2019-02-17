725f7d64f7109bc36ae4a0ec97c2fc5cf0c8043a
arunkumargovindharaj <46838557+arunkumargovindharaj@users.noreply.github.com>
2019-02-17 23:16:53 +0530

    Add files via upload

---
+++ GB102.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int b,c,d=0;
+clrscr();
+scanf("%d%d",&b,&c);
+if(b%2==0)
+{
+d=b/2;
+printf("%d",d);
+}
+  if(c%2!=0)
+  {
+  printf("%d\n",c);
+  }
+  getch();
+  }
